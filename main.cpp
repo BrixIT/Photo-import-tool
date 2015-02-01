73210598ce37aa614baffe75691e2b479834d685
Martijn Braam <martijn@brixit.nl>
2015-02-01 15:02:16 +0100

    Added search for DCIM directory

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 #include <glib.h>
 #include <gio/gio.h>
+#include <string.h>
+
+bool has_dir(GFileEnumerator *enumerator, const char *searchName){
+    GFileInfo *info;
+    while ((info = g_file_enumerator_next_file (enumerator, NULL, NULL)) != NULL)
+    {
+        const char *dirname = g_file_info_get_name (info);
+        if(strcmp(dirname,searchName) == 0){
+            return true;
+        }
+    }
+    return false;
+}
 
 int main() {
     GVolumeMonitor *volume_monitor;
@@ -23,12 +36,22 @@ int main() {
         mount = (GMount *) l->data;
         char *name = g_mount_get_name(mount);
 
-        std::cout << "Foto's importeren van " << name << std::endl;
-
         default_location = g_mount_get_default_location(mount);
         char *loc_uri = g_file_get_uri(default_location);
+        GFile *camera_root = g_file_new_for_commandline_arg(loc_uri);
 
-        std::cout << name << " " << loc_uri << std::endl;
+        // Search for DCIM directory on the device
+        GFileEnumerator *enumerator = g_file_enumerate_children (camera_root,
+                "standard::name",
+                G_FILE_QUERY_INFO_NOFOLLOW_SYMLINKS,
+                NULL,
+                NULL);
+
+        if(has_dir(enumerator, "DCIM")){
+            std::cout << "Foto's importeren van " << name << std::endl;
+        }else{
+            std::cout << name << " bevat geen DCIM map" << std::endl;
+        }
     }
 
     return 0;
