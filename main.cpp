9806fc998b95ce74d25fb6ccb2144b53ad30ffcc
Martijn Braam <martijn@brixit.nl>
2015-02-01 14:48:58 +0100

    Added mount details

--- main.cpp
+++ main.cpp
@@ -8,7 +8,9 @@ int main() {
     GList *l;
     int c;
     GMount *mount;
+    GFile *default_location;
 
+    // Get list of all gvfs mounts
     volume_monitor = g_volume_monitor_get();
     mounts = g_volume_monitor_get_mounts(volume_monitor);
     if (mounts == NULL) {
@@ -16,9 +18,17 @@ int main() {
         return 1;
     }
 
+    // Loop through all mounts and get mount details
     for (c = 0, l = mounts; l != NULL; l = l->next, c++) {
         mount = (GMount *) l->data;
-        std::cout << "CAMERA!" << std::endl;
+        char *name = g_mount_get_name(mount);
+
+        std::cout << "Foto's importeren van " << name << std::endl;
+
+        default_location = g_mount_get_default_location(mount);
+        char *loc_uri = g_file_get_uri(default_location);
+
+        std::cout << name << " " << loc_uri << std::endl;
     }
 
     return 0;
